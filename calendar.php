4d0c99a5530812dfb42879df8f2b3a8f36dde2c3
swtororphanage <swtorisfun@gmail.com>
2015-06-09 09:41:07 -0600

    center calendar

--- calendar.php
+++ calendar.php
@@ -1,2 +1,5 @@
+<div class="center">
 <iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23cc33cc&amp;src=swtorisfun%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York"
- style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
+ style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no">
+ </iframe>
+</div>
