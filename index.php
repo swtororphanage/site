54e93a03301e228e5fcbe26adb4f2a3d516ab978
swtororphanage <swtorisfun@gmail.com>
2015-06-08 22:47:15 -0600

    header

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
 <link rel="stylesheet" type="text/css" href="swtor.css">
 </head>
 <body>
-
+<div id="header">
 <h1>The Orphanage</h1>
 <?php include 'navigation.php';?>
+</div>
 <p>Showing the see through background</p>
 </body>
 </html>
