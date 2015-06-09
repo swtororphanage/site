dfaec6f53d0b4a832228977d79eacbb3a5ce8cef
swtororphanage <swtorisfun@gmail.com>
2015-06-08 22:54:47 -0600

    move navigation

--- index.php
+++ index.php
@@ -5,10 +5,8 @@
 <link rel="stylesheet" type="text/css" href="swtor.css">
 </head>
 <body>
-<div id="header">
-<h1>The Orphanage</h1>
 <?php include 'navigation.php';?>
-</div>
+
 <div id="content">
 <p>Showing the see through background</p>
 </div>
--- navigation.php
+++ navigation.php
@@ -1,13 +1,16 @@
-<div id="alignLog">
-<input type="submit" value="Sign In" name="signin">
-<input type="submit" value="Sign Up" name="signup">
-</div>
-<div id="navigation">
-<input type="submit" value="News" name="news">
-<input type="submit" value="Forums" name="forums">
-<input type="submit" value="Calender" name="calendar">
-<input type="submit" value="Guides" name="guides">
-<input type="submit" value="Officers" name="officer">
-<input type="submit" value="Profile" name="profile">
-<input type="submit" value="Contact" name="contact">
+<div id="header">
+	<h1>The Orphanage</h1>
+	<div id="alignLog">
+		<input type="submit" value="Sign In" name="signin">
+		<input type="submit" value="Sign Up" name="signup">
+	</div>
+	<div id="navigation">
+		<input type="submit" value="News" name="news">
+		<input type="submit" value="Forums" name="forums">
+		<input type="submit" value="Calender" name="calendar">
+		<input type="submit" value="Guides" name="guides">
+		<input type="submit" value="Officers" name="officer">
+		<input type="submit" value="Profile" name="profile">
+		<input type="submit" value="Contact" name="contact">
+	</div>
 </div>
