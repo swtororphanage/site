39ae1a7758c0add85b0e7b564f4f72b815449118
swtororphanage <swtorisfun@gmail.com>
2015-06-16 20:58:50 -0600

    signup

--- signup.php
+++ signup.php
@@ -1,80 +1,74 @@
-<h1>Sign Up</h1>
+<h2>Sign Up</h2>
 <form action="" method="post">
-<table style="margin: 0px auto;">
-	<tr>
-		<td align="right">Email:</td>
-		<td><input type="email" name="email"></td>
-	</tr>
-	<tr>
-		<td align="right">Password:</td>
-		<td><input type="password" name="pass"></td>
-	</tr>
-	<tr>
-		<td align="right">Re-enter Password:</td>
-		<td><input type="password" name="pass2"></td>
-	</tr>
-	<tr>
-		<td align="right">SWTOR main character name:</td>
-		<td><input type="text" name="name"></td>
-	</tr>
-	<tr>
-		<td></td>
-		<td><input type="submit" value="Create User" name="submit"></td>
-	</tr>
-</table>
+	<table style="margin: 0px auto;">
+		<tr>
+			<td align="right">Email:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td align="right">Password:</td>
+			<td><input type="password" name="pass"></td>
+		</tr>
+		<tr>
+			<td align="right">Re-enter Password:</td>
+			<td><input type="password" name="pass2"></td>
+		</tr>
+		<tr>
+			<td align="right">SWTOR main character name:</td>
+			<td><input type="text" name="name"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Create User" name="submit"></td>
+		</tr>
+	</table>
 </form>
 
 <?php
-if(isset($_POST['submit']) && isset($_POST['user']) && isset($_POST['pass'])){
-	try {
-        $link = new PDO($dsn, $username, $password, $options);
-		$sql = 'SELECT * FROM Users';
-        $stmt = $link->prepare($sql);
-        $stmt->execute();
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $stmt->closeCursor();
-	
-		$message = "";
-		foreach($users as $u){
-			if($u['name'] == $_POST['name'])
-			{
-				$message += ' Username is taken.';
+if(isset($_POST['submit']) && isset($_POST['email']) && isset($_POST['pass'])
+	&& isset($_POST['pass2']) && isset($_POST['name'])){
+	if ($_POST['pass'] == $_POST['pass2']){
+		try {
+			$link = new PDO($dsn, $username, $password, $options);
+			$sql = 'SELECT * FROM User';
+			$stmt = $link->prepare($sql);
+			$stmt->execute();
+			$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			$stmt->closeCursor();
+		
+			$message = "";
+			foreach($users as $u){
+				if($u['name'] == $_POST['name'])
+				{
+					$message += ' Username is taken.';
+				}
+				if($u['email'] == $_POST['email'])
+				{
+					$message += ' Email is taken.';
+				}
 			}
-			if($u['email'] == $_POST['email'])
-			{
-				$message += ' Email is taken.';
-			}
-		}
-	
-		if($message == ""){
-				require 'password.php';
-				$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-				$sql = "INSERT INTO `Users`(`User`, `Pass`) VALUES ('" . $_POST['user'] . "','" .$pass. "');";
+			if($message == ""){
+					require 'password.php';
+					$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+					$sql = "INSERT INTO `User`(`pass`, `email`, `swtor_name`) VALUES ("
+					.$pass.",".$_POST['email'].",".$_POST['name'].");";
 					$stmt = $link->prepare($sql);
 					$stmt->execute();
 					$stmt->closeCursor();
-				
-				$stmt = $link->prepare("SELECT `User_ID`, `User`, `Pass` FROM `Users` WHERE `User` = '".$_POST['user']."';");
-				$stmt->execute();
-					$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-					$stmt->closeCursor();
-				$usid = "";	
-				foreach($user as $u){
-					$usid = $u['User_ID'];
-				}
-				for($i = 1; $i <= 6; $i++){
-					$sql = "INSERT INTO `Bought_items`(`images_id`, `User_ID`, `bought`) VALUES (".$i.",".$usid.",0);";
-						$stmt = $link->prepare($sql);
-						$stmt->execute();
-						$stmt->closeCursor();
-				}		
 			}
-		else {
-			echo $message;
+			else {
+				echo $message;
+			}
+		}
+		catch (PDOException $e){
+			echo 'Sorry no connection, try again later';
 		}
 	}
-	catch (PDOException $e){
-		echo 'Sorry no connection, try again later';
+	else {
+		echo "Passwords were not the same, please try again";
 	}
+}
+else {
+	echo 'Please fill in all the information';
 }
 ?>
